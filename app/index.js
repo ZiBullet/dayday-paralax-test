7f863b277420cb275818eb9675ec48f4012966da
ZiBullet <zamolidinziadulaev@gmail.com>
2022-07-28 23:35:06 +0500

    Third screen is done

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
-let url = ''
-axios.get()
-let cont = document.querySelector('.cont')
-
-function reload(arr) {
-  cont.innerHTML = ''
-
-  let 
+const header = document.querySelector('.header-heading')
+let intro = document.querySelector('header');
+window.onscroll = () => {
+  let posY = window.pageYOffset || document.documentElement.scrollTop;
+  let height__intro = intro.offsetHeight; // высота intro
+  if (posY + 1 >= height__intro) { // если posY+1 > высоты intro, то появится header
+    header.classList.add('header-fixed');
+  } else {
+    header.classList.remove('header-fixed');
+  }
 }
